df74cd1803b6ed872c9e94e62086eb6e6666f999
Erik Cabezuelo Fernandez <erikcf95@gmail.com>
2026-08-25 07:50:59 +0200

    feat: add command help and exit to REPL

--- repl.go
+++ repl.go
@@ -9,6 +9,12 @@ import (
 
 const prompt = "Pokedex > "
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
@@ -27,7 +33,33 @@ func startRepl() {
 		} else {
 			command = words[0]
 		}
-		fmt.Printf("Your command was: %s\n", command)
+
+		c, ok := getCommands()[command]
+		if !ok {
+			fmt.Printf("Command %s is not a valid command\n", command)
+			continue
+		}
+
+		err := c.callback()
+		if err != nil {
+			fmt.Printf("Command errored with error: %v", err)
+			continue
+		}
+	}
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
 	}
 
 }
